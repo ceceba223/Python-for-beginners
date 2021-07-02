34668b5ea6f798aa3c6db5d5a92af6a12ae63cbe
ceceba223 <hagen-gerlatzek@web.de>
2021-07-02 20:36:02 +0200

    edited if-else file

--- basics/07-if-else.py
+++ basics/07-if-else.py
@@ -3,8 +3,10 @@ y = 5
 
 if x == y:								#if 'x' and 'y' have the same value then:
 	print("x and y are the same")
+elif x > y:								#or if 'x' is bigger then 'y'
+	print("x is bigger than y")
 else:									#in any other case:
-	print("x and y aren't the same")
+	print("x is smaller than y")
 
 
 #other operators for compairing stuff:
