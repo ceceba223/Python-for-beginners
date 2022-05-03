fb4ef8f09540e1845b9d464d686a9db6e8bf0f1a
Hagen <ceceba223@users.noreply.github.com>
2022-05-03 19:27:44 +0200

    errors everywhere broken

--- scripts/errors_everywhere.py
+++ scripts/errors_everywhere.py
@@ -1,19 +1,21 @@
-message = "6374667b596f754669786564416c6c4572726f7273217d"
-list_of_bytes = []
-list_of_ascii = []
+# Finde und behebe die 4 Errors 
 
-def decode(hex):
-    ascii_str = chr(int(hex, 16))
+message = "6374667b596f754669786564416c6c4572726f7273217d"
+list_of_bytes : []
+list_of_ascii : []
+
+def decode(hex)
+    ascii_str = chr(int(hex, 16)
     return ascii_str
 
 index = 0
 while index < len(message):
-    byte = (message[index] + message[index + 1])
-    list_of_bytes.append(byte)
-    index += 2
+byte = (message[index] + message[index + 1])
+list_of_bytes.append(byte)
+index += 2
 
 for byte in list_of_bytes:
-    ascii_char = decode(byte)
-    list_of_ascii.append(ascii_char)
+     ascii_char = decode(byte)
+list_of_ascii.append(ascii_char)
 
 print("".join(list_of_ascii))
